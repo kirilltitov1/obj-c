1a10b1d85b0409d7ba2fd54fcace179ee98cdb05
Kirill <kirill.titov@outlook.com>
2020-02-16 11:36:10 +0300

    123

---
+++ From Lessions/lesson5/lesson5/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  lesson5
+//
+//  Created by Kirill Titov on 16/01/2020.
+//  Copyright © 2020 Kirill Titov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ From Lessions/lesson5/lesson5/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lesson5
+//
+//  Created by Kirill Titov on 16/01/2020.
+//  Copyright © 2020 Kirill Titov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
