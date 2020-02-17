967089e15c6d6349c8d2ee1d6e422a16edbd5eb1
Kirill <kirill.titov@outlook.com>
2020-02-17 18:45:55 +0300

    add rootNC

--- GB Разработка под iOS на Objective-C/HW1/HW1/SceneDelegate.m
+++ GB Разработка под iOS на Objective-C/HW1/HW1/SceneDelegate.m
@@ -16,24 +16,14 @@
     
     MainVC* mainVC = [[MainVC alloc] init];
     
-    self.window.rootViewController = mainVC;
+    UINavigationController* rootNC = [[UINavigationController alloc] initWithRootViewController: mainVC];
+    
+    self.window.rootViewController = rootNC;
     
     self.window.windowScene = windowScene;
     
     [self.window makeKeyAndVisible];
-    
-    
-    
 
-//    MainVC* mainVC = [[MainVC alloc] init];
-//
-//    UINavigationController* rootNC = [[UINavigationController alloc] initWithRootViewController: mainVC];
-//
-//    self.window.rootViewController = rootNC;
-//    [self.window makeKeyAndVisible];
-//
-//    [self.window windowScene]
-    
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
