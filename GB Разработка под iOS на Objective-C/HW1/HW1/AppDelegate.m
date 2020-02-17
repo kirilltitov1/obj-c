157d6dc1064b8207de1864a001a2c2288be5172a
Kirill <kirill.titov@outlook.com>
2020-02-17 17:42:18 +0300

    проект корректно запускается . не вылетает .
    пока что с черным экраном .

--- GB Разработка под iOS на Objective-C/HW1/HW1/AppDelegate.m
+++ GB Разработка под iOS на Objective-C/HW1/HW1/AppDelegate.m
@@ -18,6 +18,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    self.window = [[UIWindow alloc] initWithFrame: [UIScreen mainScreen].bounds];
+    
+    
     return YES;
 }
 
--- GB Разработка под iOS на Objective-C/HW1/HW1/SceneDelegate.m
+++ GB Разработка под iOS на Objective-C/HW1/HW1/SceneDelegate.m
@@ -8,6 +8,9 @@
 
 
 - (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions {
+    
+//    self.window = [[UIWindow alloc] initWithWindowScene: scene];
+    
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
